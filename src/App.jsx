71a776b371ece4ae6b9b86ecdc788291237cfe0d
André Roberto <andreroberto52@gmail.com>
2022-06-02 23:35:48 -0300

    Adição de texto através do input e estilização css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,51 @@
 import { useState } from 'react'
 import { encode } from 'base-64'
 import { Meme } from './packages/meme'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  flex: 1;
+  flex-direction: column;
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  background-color: #a29bfe;
+`
+
+const Input = styled.input`
+  display: flex;
+  opacity: 0;
+  width: 0.1px;
+  height: 0.1px;
+  position: absolute;
+`
+
+const Label = styled.label`
+  display: flex;
+  position: relative;
+  width: 200px;
+  height: 50px;
+  background: #ff7675;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  font-weight: bold;
+  font-size: 1.1rem;
+  text-transform: uppercase;
+  cursor: pointer;
+  border: 2px solid #000;
+  -webkit-box-shadow: 11px 11px 8px 3px rgba(0,0,0,0.75);
+  -moz-box-shadow: 11px 11px 8px 3px rgba(0,0,0,0.75);
+  box-shadow: 11px 11px 8px 3px rgba(0,0,0,0.75);
+`
+
+const Title = styled.h1`
+  display: flex;
+  color: #fff;
+  text-align: center;
+  font-size: 3rem;
+`
 
 function App() {
   const [imageBase64, setImageBase64] = useState(null)
@@ -12,10 +57,15 @@ function App() {
   }
 
   return (
-    <div>
-      {imageBase64 ? (<Meme encodedImage={imageBase64} />) : (<input type="file" onChange={handleImage} />)}
-
-    </div>
+    <Wrapper>
+      <Title>DARK SOULS LOCATION MEME GENERATOR</Title>
+      {imageBase64 ? (<Meme encodedImage={imageBase64} />) : (
+        <div>
+          <Label for='file'>Choose a file</Label>
+          <Input id='file' type='file' onChange={handleImage} />
+        </div>
+      )}
+    </Wrapper>
   )
 }
 
--- src/packages/meme.jsx
+++ src/packages/meme.jsx
@@ -1,10 +1,60 @@
 import { useState, useEffect, useRef } from 'react'
 import { decode } from 'base-64'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  flex: 1;
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Input = styled.input`
+  padding: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  text-align: center;
+  font-size: 20px;
+`
+
+const Download = styled.label`
+  display: flex;
+  position: relative;
+  width: 200px;
+  height: 50px;
+  background: #ff7675;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  font-weight: bold;
+  font-size: 1.1rem;
+  text-transform: uppercase;
+  cursor: pointer;
+  border: 2px solid #000;
+  -webkit-box-shadow: 11px 11px 8px 3px rgba(0,0,0,0.75);
+  -moz-box-shadow: 11px 11px 8px 3px rgba(0,0,0,0.75);
+  box-shadow: 11px 11px 8px 3px rgba(0,0,0,0.75);
+`
 
 
 export function Meme(encodedImage){
+  const [text, setText] = useState('')
 
   const myCanvas = useRef()
+
+  const handleText = (textValue) => {
+    setText(textValue)
+  }
+
+  const download = () => {
+    const canvas = myCanvas.current
+    const image = canvas.toDataURL('image/png')
+    var link = document.createElement('a')
+    link.download = 'meme.png'
+    link.href = image
+    link.click()
+  }
 
   useEffect(() => {
     if(encodedImage){
@@ -14,12 +64,32 @@ export function Meme(encodedImage){
       image.src = decode(encodedImage.encodedImage)
       image.onload = () => {
         context.drawImage(image, 0, 0, 1280, 720)
+        if(text){
+          context.beginPath()
+          context.textBaseline = 'top'
+          context.fillStyle = '#FFF'
+          context.strokeStyle = '#FFF'
+          context.font = "90px EB Garamond"
+          context.textBaseline = 'middle'
+          context.textAlign = "center"
+          context.fillText(text, 1280/2, 400/2)
+          context.moveTo(1100, 230)
+          context.lineTo(200, 230)
+          context.lineWidth = 5
+          context.stroke()
+        }
+        
       }
     }
     
-  }, [encodedImage])
+  }, [encodedImage, text])
 
   return (
-    <canvas ref={myCanvas} width={1280} height={720} />
+    <Wrapper>
+      <canvas ref={myCanvas} width={1280} height={720} />
+      <Input type="text" value={text} onChange={e => {handleText(e.target.value)}} placeholder={'Type here place name'} />
+      {encodedImage && text && <Download onClick={download}> Download </Download>}
+    </Wrapper>
+    
   )
 }
